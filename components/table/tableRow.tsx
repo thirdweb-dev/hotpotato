2397bebb969e2bd23eaf3bfcdd800f4484f10d60
Samina <saminacodes@icloud.com>
2022-02-24 12:20:32 -0700

    fixed time held formatting

--- components/table/tableRow.tsx
+++ components/table/tableRow.tsx
@@ -5,20 +5,35 @@ interface TableRowProps {
   info: PlayerInfo;
 }
 
+//formatting for time spent
+function msToTime(duration) {
+  const milliseconds = Math.floor((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+  const hoursString = hours < 10 ? "0" + hours : hours;
+  const minutesString = minutes < 10 ? "0" + minutes : minutes;
+  const secondsString = seconds < 10 ? "0" + seconds : seconds;
+
+  return hoursString + ":" + minutesString + ":" + secondsString;
+}
+
 export const TableRow: React.FC<TableRowProps> = ({ info }) => {
   if (!info) {
     return null;
   }
 
+  //formatting for date
   const date = new Date(info.transferedAt);
 
   return (
     <Tr>
-      <Td>@{info.twitterHandle}</Td>
+      <Td>{info.twitterHandle}</Td>
       <Td>{info.address}</Td>
       <Td>{date.toLocaleDateString()}</Td>
       <Td>{date.toLocaleTimeString()}</Td>
-      <Td>{info.timeSpent / (1000 * 60 * 60)}</Td>
+      <Td>{msToTime(info.timeSpent)}</Td>
     </Tr>
   );
 };
